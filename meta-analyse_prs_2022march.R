02866ac737c6a626750d1bb9b3c21240136206bd
Mila Zudina <mzudina@Milas-MBP.fritz.box>
2022-03-31 22:39:14 +0100

    used to write PRS_meta_22-03-31.txt

--- meta-analyse_prs_2022march.R
+++ meta-analyse_prs_2022march.R
@@ -6,6 +6,7 @@ library(dplyr)
 library(data.table)
 
 `%nin%` = Negate(`%in%`)
+setwd("~/Dropbox/PRS_clean_run/PRS_meta-anlysis/2022march/")
 
 # first, combine the files
 df_epic = fread("~/Dropbox/PRS_clean_run/PRS_meta-anlysis/2022march/epic_prs_for_m-a.csv")
@@ -24,34 +25,39 @@ df_ukbb[, colnames(df_ukbb)[colnames(df_ukbb) %nin% colnames(df_epic)]] = list(N
 
 df = rbind(df_ukbb, df_epic)
 
-# this is for t2d & groups
-# here we need to exclude all with the Weighting == UKBB and then rename UKBB without ambiguous as UKBB
+df %>% select(where(is.character)) %>% apply(., 2, function(x){levels(as.factor(x))})
+df$Weighting[df$Weighting == "Betas"] = "Weighted"
+
+df = df %>% mutate(across(is.character, factor))
+
+levels(df$Outcome)[levels(df$Outcome)=="bc2"] <- "Breast Cancer"
+levels(df$Outcome)[levels(df$Outcome)=="crc"] <- "Colorectal Cancer"
+levels(df$Outcome)[levels(df$Outcome)=="panc"] <- "Pancreatic Cancer"
+levels(df$Outcome)[levels(df$Outcome)=="prc"] <- "Prostate Cancer"
+levels(df$Outcome)[levels(df$Outcome)=="T2D.all"] <- "T2D"
+
+levels(df$SNPs)[levels(df$SNPs)=="brc"] <- "Breast cancer"
+levels(df$SNPs)[levels(df$SNPs)=="crc"] <- "Colorectal cancer"
+levels(df$SNPs)[levels(df$SNPs)=="panc"] = "Pancreatic cancer"
+levels(df$SNPs)[levels(df$SNPs)=="prc"] = "Prostate cancer"
+levels(df$SNPs)[levels(df$SNPs)=="group1_464"] = "1a. Higher Adiposity and decreased Sex Hormones (N = 464)​"
+levels(df$SNPs)[levels(df$SNPs)=="group5_36"] = "1b. Higher Adiposity, Insulin Resistance, Lower Age at Menarche and decreased Sex Hormones (N = 36)"
+levels(df$SNPs)[levels(df$SNPs)=="group4_78"] = "2. Reduced b-cell function (N=78)"
+levels(df$SNPs)[levels(df$SNPs)=="group2_409"] = "3. Decreased lipids and Blood Pressure (N=409)"
+levels(df$SNPs)[levels(df$SNPs)=="group3_96"] = "4. Metabolic Syndrome and Insulin Resistance (N=96)"
+levels(df$SNPs)[levels(df$SNPs)=="t2d"] = "T2D"
+
+df %>% select(where(is.factor)) %>% apply(., 2, function(x){levels(as.factor(x))})
+
 df2 <- df %>% 
-  filter(Adjustment == "6 PCs", Cohort != "UKBB") %>% 
-  select(SNPs, Cohort, Weighting, Outcome, OR, lower_CI, upper_CI, P_value) %>% 
-  mutate(Beta = log(OR), 
-         SE = (upper_CI - lower_CI)/3.92,
-         CI = upper_CI - lower_CI) %>%
-  mutate(Cohort=recode_factor(Cohort, `UKBB without ambiguous SNPs` = "UKBB"))
-
-df3 <- read_excel("~/Dropbox/WCRF_Manuscript1/PRS_meta-anlysis/ukbb_and_epic_prs+new_21-10-06.xlsx", sheet = 2)
-
-# this is for cancer prss
-df4 <- df3 %>% 
-  filter(Adjustment == "6 PCs") %>%
+  filter(Adjustment == "6 PCs") %>% 
   select(SNPs, Cohort, Weighting, Outcome, OR, lower_CI, upper_CI, P_value) %>% 
   mutate(Beta = log(OR), 
          SE = (upper_CI - lower_CI)/3.92,
          CI = upper_CI - lower_CI)
 
-df5 <- rbind(df2, df4)
-
-df6 <- df5 %>% filter(!grepl("M-A", Cohort)) %>% 
-  mutate(Cohort = factor(Cohort, levels = c("UKBB", "EPIC"))) %>%
-  mutate(Outcome = factor(Outcome, levels = c("T2D", "Pancreatic Cancer", "Colorectal Cancer", "Prostate Cancer", "Breast Cancer")))
- 
-wei <- df6 %>% filter(Weighting == "Betas") 
-unwei <- df6 %>% filter(Weighting == "Unweighted")
+wei <- df2 %>% filter(Weighting == "Weighted") 
+unwei <- df2 %>% filter(Weighting == "Unweighted")
 
 wei$studlab <- paste(wei$SNPs, wei$Outcome, sep = " | ")
 unwei$studlab <- paste(unwei$SNPs, unwei$Outcome, sep = " | ")
@@ -80,8 +86,8 @@ for (i in 1:length(wei_list)) {
                    studlab = studlab,
                    data = wei_list[[i]],
                    sm = "SMD",
-                   comb.fixed = FALSE,
-                   comb.random = TRUE,
+                   fixed = TRUE,
+                   random = TRUE,
                    method.tau = "REML",
                    hakn = TRUE,
                    title = "EPIC and UKBB PRS Meta-analysis")
@@ -104,35 +110,35 @@ for (i in 1:length(wei_list)) {
 
 wei_meta <- meta
 
-write.table(wei_meta, "~/Dropbox/PRS_meta_21-10-07.txt", row.names = F, sep = "|", quote = F)
+write.table(wei_meta, "PRS_meta_22-03-31_test.txt", row.names = F, sep = "|", quote = F)
 
 
-wei_list <- split(as.data.frame(wei), f = wei$studlab)
-wei_list <- wei_list[1:(length(wei_list)-1)]
+unwei_list <- split(as.data.frame(unwei), f = unwei$studlab)
+unwei_list <- unwei_list[1:(length(unwei_list)-1)]
 
-meta <- data.frame(PRS_Outcome = c(rep(NA, length(wei_list))),
-                   fixed_OR = c(rep(NA, length(wei_list))), 
-                   fixed_lower_CI = c(rep(NA, length(wei_list))), 
-                   fixed_upper_CI = c(rep(NA, length(wei_list))), 
-                   fixed_P_value = c(rep(NA, length(wei_list))),
-                   fixed_UKBB_weight = c(rep(NA, length(wei_list))),
-                   fixed_EPIC_weight = c(rep(NA, length(wei_list))),
-                   Q = c(rep(NA, length(wei_list))),
-                   Q_pval = c(rep(NA, length(wei_list))),
-                   random_OR = c(rep(NA, length(wei_list))), 
-                   random_lower_CI = c(rep(NA, length(wei_list))), 
-                   random_upper_CI = c(rep(NA, length(wei_list))), 
-                   random_P_value = c(rep(NA, length(wei_list))))
+meta <- data.frame(PRS_Outcome = c(rep(NA, length(unwei_list))),
+                   fixed_OR = c(rep(NA, length(unwei_list))), 
+                   fixed_lower_CI = c(rep(NA, length(unwei_list))), 
+                   fixed_upper_CI = c(rep(NA, length(unwei_list))), 
+                   fixed_P_value = c(rep(NA, length(unwei_list))),
+                   fixed_UKBB_weight = c(rep(NA, length(unwei_list))),
+                   fixed_EPIC_weight = c(rep(NA, length(unwei_list))),
+                   Q = c(rep(NA, length(unwei_list))),
+                   Q_pval = c(rep(NA, length(unwei_list))),
+                   random_OR = c(rep(NA, length(unwei_list))), 
+                   random_lower_CI = c(rep(NA, length(unwei_list))), 
+                   random_upper_CI = c(rep(NA, length(unwei_list))), 
+                   random_P_value = c(rep(NA, length(unwei_list))))
 
-for (i in 1:length(wei_list)) {
+for (i in 1:length(unwei_list)) {
   
   m.gen <- metagen(TE = Beta,
                    seTE = SE,
                    studlab = studlab,
-                   data = wei_list[[i]],
+                   data = unwei_list[[i]],
                    sm = "SMD",
-                   comb.fixed = FALSE,
-                   comb.random = TRUE,
+                   fixed = TRUE,
+                   random = TRUE,
                    method.tau = "REML",
                    hakn = TRUE,
                    title = "EPIC and UKBB PRS Meta-analysis")
@@ -153,4 +159,7 @@ for (i in 1:length(wei_list)) {
   
 }
 
-wei_meta <- meta
+unwei_meta <- meta
+
+write.table(unwei_meta, "unweighted_PRS_meta_22-03-31.txt", row.names = F, sep = "|", quote = F)
+
